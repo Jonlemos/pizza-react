e5bad93f98622410ffb749521196b863ea4a00ac
Jonlemos <lemos.anjos@gmail.com>
2020-06-18 04:24:54 -0300

    Adicionando estilização da tela de login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { Button } from "@material-ui/core";
-import { ReactComponent as Logo } from "./logo.svg";
+import styled from "styled-components";
+import { Button, Grid } from "@material-ui/core";
+import { ReactComponent as MainLogo } from "./logo.svg";
 
 export default () => (
-  <>
-    <Logo />
-    <Button color="secondary">Entrar com Facebook</Button>
-  </>
+  <Container>
+    <Grid container justify="center" spacing={10}>
+      <Grid item>
+        <Logo />
+      </Grid>
+      <Grid item xs={12}>
+        <FaceBookButton variant="contained" color="secondary" fullWidth>
+          Entrar com Facebook
+        </FaceBookButton>
+      </Grid>
+    </Grid>
+  </Container>
 );
+
+const Logo = styled(MainLogo)`
+  width: 100%;
+`;
+
+const FaceBookButton = styled(Button)`
+  && {
+    font-size:20px:
+    padding: 15px;
+    text-transform: none;
+  }
+`;
+
+const Container = styled.div`
+  padding: 20px;
+`;
