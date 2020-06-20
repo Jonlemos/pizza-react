9c213db29e6fc0c010e87ccccc6682289c41199d
Jonlemos <lemos.anjos@gmail.com>
2020-06-20 04:17:52 -0300

    adicionado firebase na aplicação

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import firebase from "firebase/app";
+import "firebase/auth";
 import { Button, Grid } from "@material-ui/core";
 import { ReactComponent as MainLogo } from "./logo.svg";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCkdWm92N6brYRoz7WGFeQxrTmdBX0_3hI",
+  authDomain: "pizzariali.firebaseapp.com",
+  databaseURL: "https://pizzariali.firebaseio.com",
+  projectId: "pizzariali",
+  storageBucket: "pizzariali.appspot.com",
+  messagingSenderId: "592046647589",
+  appId: "1:592046647589:web:11401a8b1db206da3d12cc",
+  measurementId: "G-Z4CWHQL2NQ",
+};
+firebase.initializeApp(firebaseConfig);
 
 export default () => (
   <Container>
@@ -9,8 +23,16 @@ export default () => (
       <Grid item>
         <Logo />
       </Grid>
-      <Grid item xs={12}>
-        <FaceBookButton variant="contained" color="secondary" fullWidth>
+      <Grid item xs={12} container justify="center">
+        <FaceBookButton
+          onClick={() => {
+            const provider = new firebase.auth.GithubAuthProvider();
+            firebase.auth().signInWithRedirect(provider);
+          }}
+          variant="contained"
+          color="secondary"
+          fullWidth
+        >
           Entrar com Facebook
         </FaceBookButton>
       </Grid>
@@ -18,18 +40,23 @@ export default () => (
   </Container>
 );
 
+const Container = styled.div`
+  padding: 20px;
+`;
+
 const Logo = styled(MainLogo)`
   width: 100%;
 `;
 
-const FaceBookButton = styled(Button)`
+const FaceBookButton = styled(Button).attrs({
+  variant: "contained",
+  color: "secondary",
+  fullWidth: true,
+})`
   && {
-    font-size:20px:
+    font-size: 23px;
+    max-width: 480px;
     padding: 15px;
     text-transform: none;
   }
-`;
-
-const Container = styled.div`
-  padding: 20px;
 `;
