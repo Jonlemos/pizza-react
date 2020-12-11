e5967cbc4b56da41ae809d623fdc944d549b0339
unknown <lemos.anjos@gmail.com>
2020-12-11 18:08:11 -0300

    create logout

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,21 +1,49 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import styled from 'styled-components'
 import {AppBar, Toolbar, IconButton, Typography, Menu, MenuItem} from '@material-ui/core'
 import {AccountCircle} from '@material-ui/icons'
 import { ReactComponent as MainLogo } from "../login/logo.svg";
+import {AuthContext} from '../../contexts/auth'
 
-export default () => (
-  <>
-    <AppBar> 
-      <Toolbar>
-        <MainLogo />
-        <Typography color="inherit"> Olá Pessoa</Typography>
-        <IconButton color="inherit">
-          <AccountCircle/>
-        </IconButton>
-        <Menu open>
-          <MenuItem>Sair </MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
-  </>
-);
+export default () => {
+  const [anchorElement, setAnchorElement] = useState()
+  const {userInfo, logout} = useContext(AuthContext)
+  const handleOpenMenu = (e) => {
+    setAnchorElement(e.target)
+  }
+  const handleClose = () => {
+    setAnchorElement(null)
+  }
+  return (
+    <>
+      <AppBar> 
+        <Toolbar>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
+          <Typography color="inherit"> Olá {userInfo.user.displayName.split(' ')[0]}</Typography>
+          <IconButton color="inherit" onClick={handleOpenMenu}>
+            <AccountCircle/>
+          </IconButton>
+          <Menu 
+            open={!!anchorElement}
+            onClose={handleClose}
+            anchorEl={anchorElement}
+          >
+            <MenuItem onClick={logout}>Sair </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+};
+
+const LogoContainer = styled.div`
+  flex-grow:1;
+`
+
+const Logo = styled(MainLogo)`
+  width: 200px;
+  height: 50px
+`
+
